9cde7c8219e7ddcdfabc3a972e70ffc2cc2171dd
Warren Whipple <w@warrenwhipple.com>
2018-10-24 22:16:38 -0400

    Add scrollbar hiding

--- src/App.js
+++ src/App.js
@@ -1,34 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
-import { FixedSizeList as List } from 'react-window';
-import ResizeDetector from 'react-resize-detector';
+// import { FixedSizeList as List } from 'react-window';
+// import ResizeDetector from 'react-resize-detector';
+import HScroll from './HScroll';
 
-const Column = ({ index, style }) => <div style={style}>Column {index}</div>;
+// const Card = ({ index, style }) => (
+//   <div style={style} className="card">
+//     Card {index}
+//   </div>
+// );
 
-const Example = () => (
-  <ResizeDetector handleWidth>
-    {width => (
-      <div>
-        <List
-          direction="horizontal"
-          height={75}
-          itemCount={1000}
-          itemSize={100}
-          width={width ? width : 0}
-        >
-          {Column}
-        </List>
-      </div>
-    )}
-  </ResizeDetector>
-);
+// const Example = () => (
+//   <ResizeDetector handleWidth>
+//     {width => (
+//       <div>
+//         <List
+//           direction="horizontal"
+//           height={75}
+//           itemCount={1000}
+//           itemSize={100}
+//           width={width ? width : 0}
+//         >
+//           {Card}
+//         </List>
+//       </div>
+//     )}
+//   </ResizeDetector>
+// );
+
+const cardStyle = { width: 100, height: 75, flexShrink: 0 };
+
+const cards = [...Array(20).keys()].map(key => {
+  return (
+    <div key={key} className="card" style={cardStyle}>
+      Card {key}
+    </div>
+  );
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>h-scroll-test</h1>
-        <Example />
+        {/* <h1>react-window</h1>
+        <Example /> */}
+        <h1>vanilla</h1>
+        <HScroll>{cards}</HScroll>
+        <h1>hideScrollbar</h1>
+        <HScroll hideScrollbar>{cards}</HScroll>
       </div>
     );
   }
---
+++ src/HScroll.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HScroll = props => {
+  const { hideScrollbar } = props;
+  const outerStyle = {
+    overflowY: 'hidden',
+  };
+  const innerStyle = {
+    display: 'flex',
+    overflowX: 'scroll',
+    WebkitOverflowScrolling: 'touch',
+    paddingBottom: hideScrollbar ? 20 : null,
+    marginBottom: hideScrollbar ? -20 : null,
+  };
+  return (
+    <div style={outerStyle}>
+      <div style={innerStyle}>{props.children}</div>
+    </div>
+  );
+};
+
+export default HScroll;
