d588df861ad0dc65ec7e875ac5975a39e69bc7b6
Warren Whipple <w@warrenwhipple.com>
2018-12-26 16:04:44 -0500

    HScrollWIndow displays something

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import HScroll from './HScroll';
+import HScrollWindow from './HScrollWindow';
 import ResizeDetector from 'react-resize-detector';
 
 const cardHeight = 100;
 const cardWidth = 150;
+
+const Column = ({ index, style }) => (
+  <div style={style}>Column {index}</div>
+);
+
+const Card = ({ index, style }) => (
+  <div
+    style={{
+      backgroundColor: `hsl(${(index * 150) % 360}, 25%, 25%)`,
+      ...style,
+    }}
+  >
+    {index}
+  </div>
+);
 
 const cards = [...Array(20).keys()].map(key => {
   return (
@@ -24,70 +40,65 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>vanilla</h1>
         <ResizeDetector handleWidth>
           {width => (
-            <HScroll width={width} height={cardHeight} itemWidth={cardWidth}>
-              {cards}
-            </HScroll>
-          )}
-        </ResizeDetector>
-        <h1>showScrollbar</h1>
-        <ResizeDetector handleWidth>
-          {width => (
-            <HScroll
-              width={width}
-              height={cardHeight}
-              itemWidth={cardWidth}
-              showScrollbar
-            >
-              {cards}
-            </HScroll>
-          )}
-        </ResizeDetector>
-        <h1>scrollSnap leftPeek=40 rightPeek=40</h1>
-        <ResizeDetector handleWidth>
-          {width => (
-            <HScroll
-              width={width}
-              height={cardHeight}
-              itemWidth={cardWidth}
-              scrollSnap
-              leftPeek={40}
-              rightPeek={40}
-            >
-              {cards}
-            </HScroll>
-          )}
-        </ResizeDetector>
-        <h1>gap=10 growItem=0 growGap=1</h1>
-        <ResizeDetector handleWidth>
-          {width => (
-            <HScroll
-              width={width}
-              height={cardHeight}
-              itemWidth={cardWidth}
-              gap={10}
-              growItem={0}
-              growGap={1}
-            >
-              {cards}
-            </HScroll>
-          )}
-        </ResizeDetector>
-        <h1>gap=10 leftPeek=10 rightPeek=10</h1>
-        <ResizeDetector handleWidth>
-          {width => (
-            <HScroll
-              width={width}
-              height={cardHeight}
-              itemWidth={cardWidth}
-              gap={10}
-              leftPeek={10}
-              rightPeek={10}
-            >
-              {cards}
-            </HScroll>
+            <>
+              <h1>vanilla</h1>
+              <HScroll width={width} height={cardHeight} itemWidth={cardWidth}>
+                {cards}
+              </HScroll>
+              <h1>showScrollbar</h1>
+              <HScroll
+                width={width}
+                height={cardHeight}
+                itemWidth={cardWidth}
+                showScrollbar
+              >
+                {cards}
+              </HScroll>
+              <h1>scrollSnap leftPeek=40 rightPeek=40</h1>
+              <HScroll
+                width={width}
+                height={cardHeight}
+                itemWidth={cardWidth}
+                scrollSnap
+                leftPeek={40}
+                rightPeek={40}
+              >
+                {cards}
+              </HScroll>
+              <h1>gap=10 growItem=0 growGap=1</h1>
+              <HScroll
+                width={width}
+                height={cardHeight}
+                itemWidth={cardWidth}
+                gap={10}
+                growItem={0}
+                growGap={1}
+              >
+                {cards}
+              </HScroll>
+              <h1>gap=10 leftPeek=10 rightPeek=10</h1>
+              <HScroll
+                width={width}
+                height={cardHeight}
+                itemWidth={cardWidth}
+                gap={10}
+                leftPeek={10}
+                rightPeek={10}
+              >
+                {cards}
+              </HScroll>
+              <h1>React Window</h1>
+              <HScrollWindow
+                height={cardHeight}
+                itemCount={1000}
+                itemWidth={cardWidth}
+                width={width}
+              >
+                {Column}
+              </HScrollWindow>
+            </>
           )}
         </ResizeDetector>
       </div>
--- src/HScrollWindow.js
+++ src/HScrollWindow.js
@@ -1,23 +1,34 @@
+// @flow
+
 import React from 'react';
 import { FixedSizeList as List } from 'react-window';
-import ResizeDetector from 'react-resize-detector';
 
-const HScrollWindow = ({ height, itemCount, itemSize, children }) => (
-  <ResizeDetector handleWidth>
-    {width => (
-      <div>
-        <List
-          direction="horizontal"
-          height={height}
-          itemCount={itemCount}
-          itemSize={itemSize}
-          width={width ? width : 0}
-        >
-          {children}
-        </List>
-      </div>
-    )}
-  </ResizeDetector>
-);
+type Props = {
+  height: number,
+  itemCount: number,
+  itemWidth: number,
+  width: number,
+  children: any,
+};
+
+const defaultProps = {};
+
+const HScrollWindow = (props: Props) => {
+  const { height, itemCount, itemWidth, width, children } = props;
+
+  return (
+    <List
+      direction="horizontal"
+      height={height}
+      itemCount={itemCount}
+      itemSize={itemWidth}
+      width={width ? width : 0}
+    >
+      {children}
+    </List>
+  );
+};
+
+HScrollWindow.defaultProps = defaultProps;
 
 export default HScrollWindow;
